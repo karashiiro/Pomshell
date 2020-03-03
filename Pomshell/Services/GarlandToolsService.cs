dfa6223f434544db4d1201f1030a467d42628cfc
karashiiro <49822414+karashiiro@users.noreply.github.com>
2020-03-03 15:34:46 -0800

    minor gt work

--- Pomshell/Services/GarlandToolsService.cs
+++ Pomshell/Services/GarlandToolsService.cs
@@ -6,9 +6,23 @@ namespace Pomshell.Services
     {
         private readonly HttpClient _http;
 
+        private string BASE_URL = "https://www.garlandtools.org";
+        private Lang _lang;
+
         public GarlandToolsService(HttpClient http)
         {
             _http = http;
+            _lang = Lang.en;
+        }
+
+        public void SetLanguage(Lang lang) => _lang = lang;
+
+        public enum Lang
+        {
+            en,
+            fr,
+            de,
+            ja
         }
     }
 }
